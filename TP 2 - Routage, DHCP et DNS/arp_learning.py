41b7144093105b7f9d9420a27d2fbee348a53590
carambole25 <carambole258@gmail.com>
2024-12-14 20:23:39 +0100

    ajout du script de defense contre les attaques arp

---
+++ TP 2 - Routage, DHCP et DNS/arp_learning.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+import time
+
+def run_cmd(cmd):
+    return subprocess.check_output(cmd, shell=True).decode('utf-8')
+
+def get_non_permanent_lines(arp_tables):
+    return [i for i in arp_tables.splitlines() if "PERMANENT" not in i]
+
+def make_it_permanent(non_permanent_lines):
+    for line in non_permanent_lines:
+        ip = line.split()[0]
+        mac = line.split()[4]
+        run_cmd(f"ip neigh replace {ip} lladdr {mac} dev wlp4s0 nud perm")
+
+while True:
+    arp_tables = run_cmd('ip n')
+    non_permanent_lines = get_non_permanent_lines(arp_tables)
+    make_it_permanent(non_permanent_lines)
+    time.sleep()
